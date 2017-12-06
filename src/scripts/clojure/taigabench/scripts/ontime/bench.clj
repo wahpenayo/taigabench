0893b40fedcf87e57f5cdd045b971afd4cef5938
John McDonald <palisades.lakes@gmail.com>
2017-12-06 13:13:46 -0800

    partial results

--- src/scripts/clojure/taigabench/scripts/ontime/bench.clj
+++ src/scripts/clojure/taigabench/scripts/ontime/bench.clj
@@ -1,7 +1,7 @@
 (set! *warn-on-reflection* true) 
 (set! *unchecked-math* :warn-on-boxed)
 (ns ^{:author "wahpenayo at gmail dot com" 
-      :date "2017-12-04"
+      :date "2017-12-06"
       :doc "Public airline ontime data benchmark:
             https://www.r-bloggers.com/benchmarking-random-forest-implementations/
             http://stat-computing.org/dataexpo/2009/" }
@@ -21,48 +21,6 @@
 ;;----------------------------------------------------------------
 (def suffixes 
   ["8192" "65536" "524288" "4194304" "33554432"])
-(with-open [w (z/print-writer 
-                (data/output-file "l2" "taiga.results" "csv"))]
-  (.println w 
-    "model,ntrain,ntest,datatime,traintime,predicttime,rmsetime,rmse")
-  (doseq [suffix suffixes]
-    (System/gc)
-    (let [results (z/seconds
-                    (print-str "taiga" suffix)
-                    (l2/traintest 
-                      suffix taiga/mean-regression l2/prototype))
-          ^String line (s/join "," ["taiga"
-                                    (:ntrain results)
-                                    (:ntest results)
-                                    (:datatime results)
-                                    (:traintime results)
-                                    (:predicttime results)
-                                    (:rmsetime results)
-                                    (:rmse results)])]
-      (.println w line)
-      (.flush w))))
-;;----------------------------------------------------------------
-(with-open [w (z/print-writer 
-               (data/output-file "classify" "taiga.results" "csv"))]
- (.println w 
-   "model,ntrain,ntest,datatime,traintime,predicttime,auctime,auc")
- (doseq [suffix suffixes]
-   (doseq [learner [taiga/majority-vote-probability
-                    taiga/positive-fraction-probability]]
-     (System/gc)
-     (println "taiga" (cl/model-string learner) suffix)
-     (let [results (cl/traintest 
-                     suffix learner cl/prototype)
-           ^String line (s/join "," [(cl/model-string learner)
-                                     (:ntrain results)
-                                     (:ntest results)
-                                     (:datatime results)
-                                     (:traintime results)
-                                     (:predicttime results)
-                                     (:auctime results)
-                                     (:auc results)])]
-       (.println w line)
-       (.flush w)))))
 ;;----------------------------------------------------------------
 (with-open [w (z/print-writer 
                 (data/output-file "qcost" "taiga.results" "csv"))]
@@ -85,4 +43,47 @@
                                     (:qcost results)])]
       (.println w line)
       (.flush w))))
+;;----------------------------------------------------------------
+#_(with-open [w (z/print-writer 
+                (data/output-file "l2" "taiga.results" "csv"))]
+  (.println w 
+    "model,ntrain,ntest,datatime,traintime,predicttime,rmsetime,rmse")
+  (doseq [suffix suffixes]
+    (System/gc)
+    (let [results (z/seconds
+                    (print-str "taiga" suffix)
+                    (l2/traintest 
+                      suffix taiga/mean-regression l2/prototype))
+          ^String line (s/join "," ["taiga"
+                                    (:ntrain results)
+                                    (:ntest results)
+                                    (:datatime results)
+                                    (:traintime results)
+                                    (:predicttime results)
+                                    (:rmsetime results)
+                                    (:rmse results)])]
+      (.println w line)
+      (.flush w))))
+;;----------------------------------------------------------------
+#_(with-open [w (z/print-writer 
+               (data/output-file "classify" "taiga.results" "csv"))]
+ (.println w 
+   "model,ntrain,ntest,datatime,traintime,predicttime,auctime,auc")
+ (doseq [suffix suffixes]
+   (doseq [learner [taiga/majority-vote-probability
+                    taiga/positive-fraction-probability]]
+     (System/gc)
+     (println "taiga" (cl/model-string learner) suffix)
+     (let [results (cl/traintest 
+                     suffix learner cl/prototype)
+           ^String line (s/join "," [(cl/model-string learner)
+                                     (:ntrain results)
+                                     (:ntest results)
+                                     (:datatime results)
+                                     (:traintime results)
+                                     (:predicttime results)
+                                     (:auctime results)
+                                     (:auc results)])]
+       (.println w line)
+       (.flush w)))))
 ;;----------------------------------------------------------------
