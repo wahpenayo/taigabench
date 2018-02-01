cce3c6d25763d90fbb8fde1256fccbb73885c90a
John McDonald <palisades.lakes@gmail.com>
2018-01-31 18:46:12 -0800

    update taiga results

--- src/scripts/clojure/taigabench/scripts/ontime/bench.clj
+++ src/scripts/clojure/taigabench/scripts/ontime/bench.clj
@@ -1,7 +1,7 @@
 (set! *warn-on-reflection* true) 
 (set! *unchecked-math* :warn-on-boxed)
 (ns ^{:author "wahpenayo at gmail dot com" 
-      :date "2017-12-23"
+      :date "2018-01-29"
       :doc "Public airline ontime data benchmark:
             https://www.r-bloggers.com/benchmarking-random-forest-implementations/
             http://stat-computing.org/dataexpo/2009/" }
@@ -34,49 +34,49 @@
 (def suffixes 
   ["32768" "131072" "524288" "2097152" "8388608" "33554432"])
 ;;----------------------------------------------------------------
-#_(loop  [records []
-         suffixes suffixes]
-   (when-not (empty? suffixes)
-     (System/gc)
-     (let [suffix (first suffixes)
-           record (z/seconds
-                    (print-str "taiga" suffix)
-                    (l2/traintest 
-                      suffix 
-                      taiga/mean-regression 
-                      l2/prototype))
-           records (conj records (assoc record :model "taiga"))]
-       (write-csv 
-         records 
-         (data/output-file "l2" "taiga.results" "csv"))
-       (recur records (rest suffixes)))))
+(loop  [records []
+        suffixes suffixes]
+  (when-not (empty? suffixes)
+    (System/gc)
+    (let [suffix (first suffixes)
+          record (z/seconds
+                   (print-str "taiga" suffix)
+                   (l2/traintest 
+                     suffix 
+                     taiga/mean-regression 
+                     l2/prototype))
+          records (conj records (assoc record :model "taiga"))]
+      (write-csv 
+        records 
+        (data/output-file "l2" "taiga.results" "csv"))
+      (recur records (rest suffixes)))))
 ;;----------------------------------------------------------------
-#_(loop  [records []
-         suffixes suffixes]
-   (when-not (empty? suffixes)
-     (System/gc)
-     (let [suffix (first suffixes)
-           mvp (assoc 
-                 (z/seconds
-                   (print-str "taiga-mvp" suffix)
-                   (cl/traintest 
-                     suffix 
-                     taiga/majority-vote-probability 
-                     cl/prototype))
-                 :model "taiga-mvp")
-           pfp (assoc 
-                 (z/seconds
-                   (print-str "taiga-pfp" suffix)
-                   (cl/traintest 
-                     suffix 
-                     taiga/positive-fraction-probability 
-                     cl/prototype))
-                 :model "taiga-pfp")
-           records (concat records [mvp pfp])]
-       (write-csv 
-         records 
-         (data/output-file "classify" "taiga.results" "csv"))
-       (recur records (rest suffixes)))))
+(loop  [records []
+       suffixes suffixes]
+ (when-not (empty? suffixes)
+   (System/gc)
+   (let [suffix (first suffixes)
+         mvp (assoc 
+               (z/seconds
+                 (print-str "taiga-mvp" suffix)
+                 (cl/traintest 
+                   suffix 
+                   taiga/majority-vote-probability 
+                   cl/prototype))
+               :model "taiga-mvp")
+         pfp (assoc 
+               (z/seconds
+                 (print-str "taiga-pfp" suffix)
+                 (cl/traintest 
+                   suffix 
+                   taiga/positive-fraction-probability 
+                   cl/prototype))
+               :model "taiga-pfp")
+         records (concat records [mvp pfp])]
+     (write-csv 
+       records 
+       (data/output-file "classify" "taiga.results" "csv"))
+     (recur records (rest suffixes)))))
 ;;----------------------------------------------------------------
 (loop  [records []
         suffixes suffixes]
