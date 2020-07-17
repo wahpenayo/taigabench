75bb74721db2e3b52df22f4cf2ceffe3c666d1a0
John McDonald <palisades.lakes@gmail.com>
2020-07-16 17:38:40 -0700

    update for java 12

--- clj12g.bat
+++ clj12g.bat
@@ -15,7 +15,7 @@ set PROF=
 ::set PROF=-Xrunhprof:cpu=samples,depth=128,thread=y,doe=y
 
 ::set THRUPUT=-d64 -server -XX:+AggressiveOpts 
-set THRUPUT=-d64 -server
+set THRUPUT=-server
 ::set THRUPUT=
 
 ::set XMX=-Xms48g -Xmx48g -Xmn20g 
@@ -24,7 +24,7 @@ set XMX=-Xms12g -Xmx12g -Xmn5g
 set OPENS=--add-opens java.base/java.lang=ALL-UNNAMED
 set CP=-cp ./src/scripts/clojure;lib/*
 
-set JAVA_HOME=%JAVA9%
+set JAVA_HOME=%JAVA12%
 set JAVA="%JAVA_HOME%\bin\java"
 
 set CMD=%JAVA% %THRUPUT% -ea -dsa -Xbatch %GC% %PROF% %XMX% %COMPRESSED% %TRACE% %OPENS% %CP% clojure.main %*
